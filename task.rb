01ebdd05e9607ef73a4859825972d7492facd54d
finish-cloud <57353856+finish-cloud@users.noreply.github.com>
2020-05-03 13:45:55 +0900

    Q1~7まで

--- task.rb
+++ task.rb
@@ -29,7 +29,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -39,13 +39,16 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
+  number2 = numbers1.map{|x| x * 10}
+  p numbers2
 
 end
 
@@ -53,7 +56,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
