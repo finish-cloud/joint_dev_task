ba475c499f063b0bc7cd3329db7edba602e53589
finish-cloud <57353856+finish-cloud@users.noreply.github.com>
2020-05-08 00:23:47 +0900

    Q8,Q11修正

--- task.rb
+++ task.rb
@@ -97,7 +97,8 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-   sports.flatten!.uniq!.each.with_index(1) { |sport, i| puts "No#{i} #{sport}" }
+  sports.flatten!.uniq!.each.with_index(1) { |sport, i| puts "No#{i} #{sport}" }
+  puts "No#{i} #{sport}"
 end
 
 def q12
