8e351bdb44c20767ad81029a61ddeb877915ce46
finish-cloud <57353856+finish-cloud@users.noreply.github.com>
2020-05-08 18:48:54 +0900

    Q13~16

--- task.rb
+++ task.rb
@@ -112,14 +112,14 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  p user_data.merge!(update_data)
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  p array = data.keys
 end
 
 def q15
@@ -127,7 +127,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  puts data1.has_key?(:age) ? "OK" : "NG"
+  puts data2.has_key?(:age) ? "OK" : "NG"
 end
 
 def q16
@@ -139,7 +140,7 @@ def q16
   ]
 
   # 以下に回答を記載
-
+   users.each { |user| puts "「私の名前は#{user[:name]}です。年齢は#{user[:age]}です。」"}
 end
 
 class UserQ17
